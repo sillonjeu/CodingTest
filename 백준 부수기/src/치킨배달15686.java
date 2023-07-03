2e5ff6e838e21eb127007cc33a58e3b56cb635d3
sillonjeu <pius0316@naver.com>
2023-07-03 11:10:35 +0900

    백준 부수기 7/3

--- 백준 부수기/src/치킨배달15686.java
+++ 백준 부수기/src/치킨배달15686.java
@@ -5,6 +5,13 @@ import java.util.Comparator;
 import java.util.StringTokenizer;
 
 public class 치킨배달15686 {
+  static int N, M;
+  static int[][] arr;
+  static ArrayList<int[]> chickenList;
+  static ArrayList<int[]> houseList;
+  static boolean[] selected;
+  static int answer = Integer.MAX_VALUE;
+
   public static void main(String[] args) throws Exception {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
@@ -12,62 +19,71 @@ public class 치킨배달15686 {
     StringTokenizer st = new StringTokenizer(br.readLine(), " ");
 
     // N, M 입력받기
-    int N = Integer.parseInt(st.nextToken());
-    int M = Integer.parseInt(st.nextToken());
+    N = Integer.parseInt(st.nextToken());
+    M = Integer.parseInt(st.nextToken());
 
-    int[][] arr = new int[N][N];
-    ArrayList<int[]> list2 = new ArrayList<>();
-    ArrayList<int[]> list1 = new ArrayList<>();
-    ArrayList<int[]> result = new ArrayList<>();
-    ArrayList<int[]> result2 = new ArrayList<>();
+    arr = new int[N][N];
+    chickenList = new ArrayList<>();
+    houseList = new ArrayList<>();
 
-    // 2차원 배열
+    // 2차원 배열 입력받기
     for (int i = 0; i < N; i++) {
       StringTokenizer st1 = new StringTokenizer(br.readLine(), " ");
       for (int j = 0; j < N; j++) {
         arr[i][j] = Integer.parseInt(st1.nextToken());
-
         if (arr[i][j] == 2) {
-          int[] coordinated = { i, j };
-          list2.add(coordinated);
+          int[] coordinate = { i, j };
+          chickenList.add(coordinate);
         } else if (arr[i][j] == 1) {
-          int[] coordinated = { i, j };
-          list1.add(coordinated);
+          int[] coordinate = { i, j };
+          houseList.add(coordinate);
         }
       }
     }
 
-    // 2에서부터 1들의 거리 합한 것
-    for (int[] List2 : list2) {
-      int x = List2[0];
-      int y = List2[1];
-      int cal = 0;
-      for (int[] List1 : list1) {
-        cal += Math.abs(List1[0] - x) + Math.abs(List1[1] - y);
-      }
-      int[] Result = { cal, x, y, 0 };
-      result.add(Result);
+    selected = new boolean[chickenList.size()];
+    chooseChicken(0, 0);
+
+    sb.append(answer);
+
+    bw.write(sb.toString());
+    bw.flush();
+    bw.close();
+    br.close();
+  }
+
+  // 폐업시킬 치킨 집 선택하는 함수
+  public static void chooseChicken(int start, int count) {
+    if (count == M) {
+      int chickenDistance = calculateChickenDistance();
+      answer = Math.min(answer, chickenDistance);
+      return;
     }
 
-    for(int[] List1 : list1) {
-      int x = List1[0];
-      int y = List1[1];
-      int cal = 100;
-      for(int[] Result : result) {
-        if (cal >= Math.abs(List1[0] - x) + Math.abs(List1[1] - y)) {
-          cal = Math.abs(List1[0] - x) + Math.abs(List1[1] - y);
+    for (int i = start; i < chickenList.size(); i++) {
+      selected[i] = true;
+      chooseChicken(i + 1, count + 1);
+      selected[i] = false;
+    }
+  }
+
+  // 선택된 치킨 집으로부터의 거리 합을 계산하는 함수
+  public static int calculateChickenDistance() {
+    int totalDistance = 0;
+
+    for (int[] house : houseList) {
+      int minDistance = Integer.MAX_VALUE;
+
+      for (int i = 0; i < chickenList.size(); i++) {
+        if (selected[i]) {
+          int[] chicken = chickenList.get(i);
+          int distance = Math.abs(house[0] - chicken[0]) + Math.abs(house[1] - chicken[1]);
+          minDistance = Math.min(minDistance, distance);
         }
       }
+      totalDistance += minDistance;
     }
 
-    Collections.sort(result, new Comparator<int[]>() {
-      @Override
-      public int compare(int[] arr1, int[] arr2) {
-        return arr2[0] - arr1[0];
-      }
-    });
-    // for (int[] coordinates : result) {System.out.println("(" + coordinates[0] +
-    // ", " + coordinates[1] + ", " + coordinates[2] +")");}
-
+    return totalDistance;
   }
 }
