bd143340902fb24020473442f0c83f30a23f873f
sillonjeu <pius0316@naver.com>
2023-07-17 01:01:34 +0900

    백준 부수기 7/17

---
+++ 백준 부수기/src/수강신청13414.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.LinkedHashSet;
+import java.util.StringTokenizer;
+
+public class 수강신청13414 {
+  public static void main(String[] args) throws NumberFormatException, IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    int K = Integer.parseInt(st.nextToken());
+    int L = Integer.parseInt(st.nextToken());
+    int count = 0;
+
+    LinkedHashSet<String> set = new LinkedHashSet<>();
+
+    for (int i = 0; i < L; i++) {
+      String str = br.readLine();
+      if (set.contains(str))
+        set.remove(str);
+      set.add(str);
+    }
+    for (String x : set) {
+      count++;
+      System.out.println(x);
+      if (count == K)
+        break;
+    }
+  }
+}
