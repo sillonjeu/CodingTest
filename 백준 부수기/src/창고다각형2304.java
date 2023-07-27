1d5ea1462420f510aab9b3f05cae9fe73b461aad
sillonjeu <pius0316@naver.com>
2023-07-27 23:13:05 +0900

    백준 부수기 7/27

---
+++ 백준 부수기/src/창고다각형2304.java
@@ -0,0 +1,10 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class 창고다각형2304 {
+  public static void main(String[] args) throws Exception {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    
+  }
+}
