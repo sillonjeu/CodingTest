c2b2d08359c479aa944e15649f5034940c3879bd
sillonjeu <pius0316@naver.com>
2024-05-09 23:50:45 +0900

    2024/05/09

--- 백준 부수기/src/프로그래머스/5.4~5.10/3_2차원 동전 뒤집기.js
+++ 백준 부수기/src/프로그래머스/5.4~5.10/3_2차원 동전 뒤집기.js
@@ -1,4 +1,33 @@
 function solution(beginning, target) {
     var answer = 0;
+    
+    // 행부터 보기
+    for(let i = 0; i < beginning.length; i++) {
+        if(beginning[i][0] != target[i][0]) {
+            answer++;
+            for(let j = 0; j < beginning.length; j++) {
+                beginning[i][j] = (beginning[i][j] == 0 ? 1 : 0);
+            }
+        }
+    }
+    
+    for(let i = 0; i < beginning.length; i++) {
+        if(beginning[0][i] != target[0][i]) {
+            answer++;
+            for(let j = 0; j < beginning.length; j++) {
+                beginning[j][i] = (beginning[j][i] == 0 ? 1 : 0);
+            }
+        }
+    }
+    
+    for(let i = 0; i < beginning.length; i++) {
+        for(let j = 0; j < beginning.length; j++) {
+            if(beginning[i][j] != target[i][j]) {
+                answer = -1;
+                break;
+            }
+        }
+    }
+    
     return answer;
 }
